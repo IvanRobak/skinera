c174b60585f5855c2c77da1dfb6edfe4699dc287
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2025-02-19 11:16:48 +0200

    ProductsPage aside

--- src/features/products/ProductList.tsx
+++ src/features/products/ProductList.tsx
@@ -7,18 +7,20 @@ const ProductList = ({
   searchQuery,
   selectedBrand,
   selectedCategory,
+  selectedCountry,
 }: {
   sortOption: string;
   searchQuery: string;
   selectedBrand: string;
   selectedCategory: string;
+  selectedCountry: string;
 }) => {
   const [filteredProducts, setFilteredProducts] = useState(products);
 
   useEffect(() => {
     let updatedProducts = [...products];
 
-    // Фільтр за пошуковим запитом
+    // Фільтр за пошуком
     if (searchQuery) {
       updatedProducts = updatedProducts.filter(product =>
         product.name.toLowerCase().includes(searchQuery.toLowerCase())
@@ -35,6 +37,11 @@ const ProductList = ({
       updatedProducts = updatedProducts.filter(product => product.category === selectedCategory);
     }
 
+    // Фільтр за країною
+    if (selectedCountry) {
+      updatedProducts = updatedProducts.filter(product => product.country === selectedCountry);
+    }
+
     // Сортування
     if (sortOption === 'price-asc') {
       updatedProducts.sort((a, b) => a.price - b.price);
@@ -42,20 +49,19 @@ const ProductList = ({
       updatedProducts.sort((a, b) => b.price - a.price);
     } else if (sortOption === 'name') {
       updatedProducts.sort((a, b) => a.name.localeCompare(b.name));
+    } else if (sortOption === 'country') {
+      updatedProducts.sort((a, b) => a.country.localeCompare(b.country));
     }
 
     setFilteredProducts(updatedProducts);
-  }, [sortOption, searchQuery, selectedBrand, selectedCategory]);
+  }, [sortOption, searchQuery, selectedBrand, selectedCategory, selectedCountry]);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 gap-y-16 gap-x-6 relative">
       {filteredProducts.length > 0 ? (
         filteredProducts.map(product => (
           <div key={product.id} className="relative group">
-            {/* Картка товару */}
             <ProductCard product={product} />
-
-            {/* Кнопка "Купити" в просторі між рядами */}
             <div className="absolute left-1/2 -translate-x-1/2 bottom-[-50px] opacity-0 group-hover:opacity-100 group-hover:bottom-[-40px] transition-all duration-300">
               <button className="bg-pink-500 text-white py-2 px-16 rounded-lg shadow-md hover:bg-pink-600 transition">
                 Купити
