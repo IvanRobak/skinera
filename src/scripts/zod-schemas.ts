db2a40adbeabe5f9e0074386ce4d979575a92f8e
yabluko <yurarobak7@gmail.com>
2025-07-02 14:30:23 +0300

    fix: bug with email

--- src/scripts/zod-schemas.ts
+++ src/scripts/zod-schemas.ts
@@ -2,14 +2,19 @@ import { z } from 'zod';
 
 export const RegisterSchema = z.object({
     name: z.string().min(1, { message: `Поле Ім'я обов'язкове` }),
-    email: z.string().email({ message: `Поле електронна адреса обов'язкове` }),
+    email: z.string()
+        .min(1, { message: `Поле електронна адреса обов'язкове` })
+        .email({ message: `Введіть коректну електронну адресу` }),
     password: z.string({ message: `Поле Пароль обов'язкове` })
+        .min(1, { message: `Поле з паролем обов'язкове` })
         .min(8, { message: 'Пароль має бути не менше 8 символів' })
         .max(20, { message: 'Пароль має бути не більше 20 символів' }),
 }).required()
 
 export const SignInSchema = z.object({
-    email: z.string().email({ message: `Поле електронна адреса обов'язкове` }),
+    email: z.string()
+        .min(1, { message: `Поле електронна адреса обов'язкове` })
+        .email({ message: `Введіть коректну електронну адресу` }),
     password: z.string({ message: `Поле Пароль обов'язкове` })
         .min(8, { message: 'Пароль має бути не менше 8 символів' })
         .max(20, { message: 'Пароль має бути не більше 20 символів' }),
