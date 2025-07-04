39c92b62de680aa354fccb5d37ef082f6b9fa535
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2025-07-04 11:08:32 +0300

    Remove buy buttons from ProductList component

--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -2,8 +2,6 @@
 
 import { useState, useEffect } from 'react';
 import ProductCard from './ProductCard';
-import { useCartStore } from '../store/cartStore'; // Імпортуй зі свого шляху
-import { toast } from 'react-toastify';
 
 interface Product {
   id: number;
@@ -35,7 +33,6 @@ const ProductList = ({
   limit?: number;
 }) => {
   const [products, setProducts] = useState<Product[]>(initialProducts);
-  const { addToCart } = useCartStore();
 
   // Оновлення продуктів, якщо змінилися фільтри (хоча сортування вже на сервері)
   useEffect(() => {
@@ -63,39 +60,12 @@ const ProductList = ({
     fetchProducts();
   }, [sortOption, searchQuery, selectedBrand, selectedCategory, selectedCountry, page, limit]);
 
-  // Обробник для додавання товару до кошика
-  const handleAddToCart = (product: Product) => {
-    const cartItem = {
-      id: product.id,
-      name: product.name,
-      price: product.price,
-      image_url: product.image_url,
-      quantity: 1,
-    };
-    addToCart(cartItem);
-    toast.success('Товар додано до кошика!', {
-      position: 'top-right',
-      autoClose: 1000,
-    });
-  };
-
   return (
     <div className="w-full flex flex-col items-center sm:grid sm:grid-cols-2 md:grid-cols-4 gap-10 sm:gap-6 p-4 sm:p-6 sm:justify-items-center">
       {products.length > 0 ? (
         products.map(product => (
           <div key={product.id} className="relative group w-full max-w-xs">
             <ProductCard product={product} />
-            <div className="absolute left-1/2 -translate-x-1/2 bottom-[-10px] opacity-0 group-hover:opacity-100 group-hover:bottom-[-20px] transition-all duration-300 z-10">
-              <button
-                className="bg-pink-500 text-white py-1 px-12 rounded-lg shadow-md hover:bg-pink-600 transition text-xs sm:text-sm"
-                onClick={e => {
-                  e.stopPropagation();
-                  handleAddToCart(product);
-                }}
-              >
-                Купити
-              </button>
-            </div>
           </div>
         ))
       ) : (
