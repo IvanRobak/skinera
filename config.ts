b3843f77ddfa255d228047dff3c74a5859d5bff6
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2025-08-14 12:47:08 +0300

    removed mongo

--- config.ts
+++ config.ts
@@ -1,15 +1,13 @@
-import { z } from 'zod'
-import dotenv from "dotenv"
+import { z } from 'zod';
+import dotenv from 'dotenv';
 
-
-dotenv.config({ path : '.env.local' })
+dotenv.config({ path: '.env.local' });
 
 const ENVSchema = z.object({
-    MONGODB_URI: z.string(),
-    NEXTAUTH_SECRET: z.string(),
-    MERCHANT_ACCOUNT: z.string(),
-    MERCHANT_SECRET_KEY: z.string(),
-    WAYFORPAY_API: z.string(),
-})
+  NEXTAUTH_SECRET: z.string(),
+  MERCHANT_ACCOUNT: z.string(),
+  MERCHANT_SECRET_KEY: z.string(),
+  WAYFORPAY_API: z.string(),
+});
 
-export const config = ENVSchema.parse(process.env)
+export const config = ENVSchema.parse(process.env);
