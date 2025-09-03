2ca2185c95bacce3111caf682b54ba6ce182e145
yabluko <yurarobak7@gmail.com>
2025-09-03 17:14:46 +0300

    fix: added static geneeration

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,6 @@
 import { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = 'https://skinera.com.ua'; // Update with your actual domain
 
